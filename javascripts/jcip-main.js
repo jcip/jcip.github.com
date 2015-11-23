c1589ffa775af59932dd4615b2204c27f0becd66
Tim Peierls <tim@peierls.net>
2015-11-23 17:28:14 -0500

    author link active/disabled status

--- javascripts/jcip-main.js
+++ javascripts/jcip-main.js
@@ -1,12 +1,19 @@
 $(function() {
     $('header.navbar').load('/include/jcip-header.html', function() {
         var path = location.pathname.split('/').slice(-1)[0];
-        if (path === "faq.html") {
-            $('#faq-link').addClass('active');
-            $('#faq-link a').addClass('disabled');
-        } else {
-            $('#faq-link a').removeClass('disabled');
+        
+        function initLink(page, id) {
+            var linkSelector = '#' + id,
+                aSelector = linkSelector + ' a'
+                ;
+            if (path === page) {
+                $(linkSelector).addClass('active');
+                $(aSelector).addClass('disabled');
+            }
         }
+        
+        initLink("faq.html");
+        initLink("authors.html");
     });
     $('.footer').load('/include/jcip-footer.html');
 });
