991854a6cb7bb3b05ac80759981cf34b88e4b034
Tim Peierls <tim@peierls.net>
2015-11-23 12:42:19 -0500

    Move included files to separate dir

--- javascripts/jcip-main.js
+++ javascripts/jcip-main.js
@@ -1,4 +1,4 @@
 $(function() {
-    $('header.navbar').load('/jcip-header.html');
-    $('.footer').load('/jcip-footer.html');
+    $('header.navbar').load('/include/jcip-header.html');
+    $('.footer').load('/include/jcip-footer.html');
 });
