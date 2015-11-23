b2a0da0e7ab53827624ff5fc900b3fa224033025
Tim Peierls <tim@peierls.net>
2015-11-23 17:39:22 -0500

    load timing

--- javascripts/jcip-main.js
+++ javascripts/jcip-main.js
@@ -1,19 +1,18 @@
 $(function() {
-    $('header.navbar').load('/include/jcip-header.html', function() {
-        var path = location.pathname.split('/').slice(-1)[0];
-        
-        function initLink(page, id) {
-            var linkSelector = '#' + id,
-                aSelector = linkSelector + ' a'
-                ;
-            if (path === page) {
-                $(linkSelector).addClass('active');
-                $(aSelector).addClass('disabled');
-            }
+    var path = location.pathname.split('/').slice(-1)[0];
+    function initLink(page, id) {
+        var linkSelector = '#' + id,
+            aSelector = linkSelector + ' a'
+            ;
+        if (path === page) {
+            $(linkSelector).addClass('active');
+            $(aSelector).addClass('disabled');
         }
-        
+    }
+    $('header.navbar').load('/include/jcip-header.html', function() {
         initLink("faq.html", "faq-link");
+    });
+    $('.footer').load('/include/jcip-footer.html', function() {
         initLink("authors.html", "author-link");
     });
-    $('.footer').load('/include/jcip-footer.html');
 });
