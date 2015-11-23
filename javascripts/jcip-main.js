93e737361c5d0200ed7e3f2ce77daef0f7e2f30d
Tim Peierls <tim@peierls.net>
2015-11-23 13:05:27 -0500

    Active link for FAQ

--- javascripts/jcip-main.js
+++ javascripts/jcip-main.js
@@ -1,4 +1,9 @@
 $(function() {
-    $('header.navbar').load('/include/jcip-header.html');
+    $('header.navbar').load('/include/jcip-header.html', function() {
+        var path = location.pathname.split('/').slice(-1)[0];
+        if (path === "faq.html") {
+            $('faq-link').addClass('active');
+        }
+    });
     $('.footer').load('/include/jcip-footer.html');
 });
