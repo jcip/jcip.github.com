6594eb781d5d727ed5e08ce5a456c9715177b362
Tim Peierls <tim@peierls.net>
2015-11-23 14:47:30 -0500

    typo in selector

--- javascripts/jcip-main.js
+++ javascripts/jcip-main.js
@@ -2,7 +2,7 @@ $(function() {
     $('header.navbar').load('/include/jcip-header.html', function() {
         var path = location.pathname.split('/').slice(-1)[0];
         if (path === "faq.html") {
-            $('faq-link').addClass('active');
+            $('#faq-link').addClass('active');
         }
     });
     $('.footer').load('/include/jcip-footer.html');
